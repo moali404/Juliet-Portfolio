22721e1c1d53915203720d862111274422b80f7c
Mohammad Ali <mohammad.ali@1und1.de>
2024-08-06 12:43:08 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      transitionProperty: {
+        height: 'height',
+      },
+      keyframes: {
+        loopTextLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        loopL: 'loopTextLeft  4s linear infinite',
+      },
+      colors: {
+        orange: '#ed7c50',
+        darkOrange: '#d96d43',
+        darkGrey: '#595959',
+        lightGrey: '#8a8a8a',
+        green: '#43ae61',
+        purple: '#8566f6',
+        turks: '#a6e2e3',
+        red: '#e46060',
+        black: '#282829',
+        white: '#f5f4f5',
+      },
+    },
+  },
+  plugins: [],
+};
